12c89a9520df802963ffec01c2ecd4a0656a910d
Léo Salé <leo.sale72@gmail.com>
2021-05-08 18:17:25 +0200

    feat: add signin feature (#2)

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -3,11 +3,15 @@ import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import User from 'App/Models/User'
 
 export default class UsersController {
-  public async signup({ view }: HttpContextContract) {
-    return view.render('signup')
+  public async signup({ view, auth, response }: HttpContextContract) {
+    if (auth.isLoggedIn) {
+      response.redirect('/')
+    } else {
+      return view.render('signup')
+    }
   }
 
-  public async signin({ request, response, session }: HttpContextContract) {
+  public async signin({ request, response, session, auth }: HttpContextContract) {
     const newSignupSchema = schema.create({
       username: schema.string({ trim: true }, [
         rules.unique({ table: 'users', column: 'username', caseInsensitive: true }),
@@ -22,11 +26,31 @@ export default class UsersController {
         },
       })
 
-      await User.create(payload)
-      response.redirect('/signup')
+      const user = await User.create(payload)
+      await auth.login(user)
+      response.redirect('/')
     } catch (error) {
       session.flash('errors', error.messages)
       response.redirect('/signup')
+    }
+  }
+
+  public async login({ view, auth, response }: HttpContextContract) {
+    if (auth.isLoggedIn) {
+      response.redirect('/')
+    } else {
+      return view.render('login')
+    }
+  }
+
+  public async logging({ request, response, auth, session }: HttpContextContract) {
+    try {
+      const { username, password } = await request.only(['username', 'password'])
+      await auth.attempt(username, password)
+      response.redirect('/')
+    } catch (error) {
+      session.flash('errors', { login: 'Username or Password incorrect' })
+      response.redirect('/login')
     }
   }
 }
--- test/controller/UsersController.spec.ts
+++ test/controller/UsersController.spec.ts
@@ -2,6 +2,8 @@ import test from 'japa'
 import { JSDOM } from 'jsdom'
 import supertest from 'supertest'
 import User from 'App/Models/User'
+import Database from '@ioc:Adonis/Lucid/Database'
+
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
@@ -16,13 +18,20 @@ test.group('UsersController', () => {
     })
   })
 
-  test.group('Signin', () => {
+  test.group('Signin', (group) => {
+    group.beforeEach(async () => {
+      await Database.beginGlobalTransaction()
+    })
+    group.afterEach(async () => {
+      await Database.rollbackGlobalTransaction()
+    })
     test('should create a new user if params are valid', async (assert) => {
       const usersBefore = (await User.all()).length
       await supertest(BASE_URL)
         .post('/signup')
         .send({ username: 'username', password: 'password', password_confirmation: 'password' })
         .expect(302)
+        .expect('Location', '/')
       const usersAfter = (await User.all()).length
       assert.equal(usersAfter - usersBefore, 1)
     })
@@ -33,6 +42,43 @@ test.group('UsersController', () => {
         .send({})
         .expect(302)
         .expect('Location', '/signup')
+
+      assert.isTrue(redirect)
+    })
+  })
+
+  test.group('Login', () => {
+    test('ensure login page works', async (assert) => {
+      const { text } = await supertest(BASE_URL).get('/login').expect(200)
+      const { document } = new JSDOM(text).window
+      const title = document.querySelector('h1')
+      assert.exists(title)
+      assert.equal(title!.textContent!.trim(), 'Log in')
+    })
+  })
+
+  test.group('Logging', (group) => {
+    group.beforeEach(async () => {
+      await Database.beginGlobalTransaction()
+    })
+    group.afterEach(async () => {
+      await Database.rollbackGlobalTransaction()
+    })
+    test('should log in a user if params are valid', async (assert) => {
+      await User.create({ username: 'username', password: 'password' })
+      await supertest(BASE_URL)
+        .post('/login')
+        .send({ username: 'username', password: 'password' })
+        .expect(302)
+        .expect('Location', '/')
+    })
+
+    test('should redirect back if params are invalid', async (assert) => {
+      const { redirect } = await supertest(BASE_URL)
+        .post('/login')
+        .send({})
+        .expect(302)
+        .expect('Location', '/login')
 
       assert.isTrue(redirect)
     })

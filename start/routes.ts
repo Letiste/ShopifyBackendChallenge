4e6b29441b1b74d034e984b6756e9d3fedb31578
Léo Salé <leo.sale72@gmail.com>
2021-05-08 18:16:59 +0200

    feat: add signin feature

--- start/routes.ts
+++ start/routes.ts
@@ -7,3 +7,7 @@ Route.get('/', async ({ view }) => {
 Route.get('/signup', 'UsersController.signup')
 
 Route.post('/signup', 'UsersController.signin')
+
+Route.get('/login', 'UsersController.login')
+
+Route.post('/login', 'UsersController.logging')

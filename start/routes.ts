904689916d66c91f65cf88b473988050d368dae1
Léo Salé <leo.sale72@gmail.com>
2021-05-08 18:28:40 +0200

    feat: add logout feature (#3)

--- start/routes.ts
+++ start/routes.ts
@@ -1,7 +1,11 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async ({ view }) => {
-  return view.render('welcome')
+Route.get('/', async ({ view, auth, response }) => {
+  if (auth.isLoggedIn) {
+    return view.render('welcome')
+  } else {
+    response.redirect('/login')
+  }
 })
 
 Route.get('/signup', 'UsersController.signup')
@@ -11,3 +15,5 @@ Route.post('/signup', 'UsersController.signin')
 Route.get('/login', 'UsersController.login')
 
 Route.post('/login', 'UsersController.logging')
+
+Route.delete('/logout', 'UsersController.logout')

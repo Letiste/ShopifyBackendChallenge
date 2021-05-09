c990ae1ac0d635b1a202b398790b7859469db522
Léo Salé <leo.sale72@gmail.com>
2021-05-09 20:12:17 +0200

    fix: update env var validation for heroku

--- env.ts
+++ env.ts
@@ -15,8 +15,11 @@
 import Env from '@ioc:Adonis/Core/Env'
 
 export default Env.rules({
-  HOST: Env.schema.string({ format: 'host' }),
-  PORT: Env.schema.number(),
+  HOST:
+    Env.get('NODE_ENV') === 'production'
+      ? Env.schema.string.optional({ format: 'host' })
+      : Env.schema.string({ format: 'host' }),
+  PORT: Env.get('NODE_ENV') === 'production' ? Env.schema.number.optional() : Env.schema.number(),
   APP_KEY: Env.schema.string(),
   APP_NAME: Env.schema.string(),
   CACHE_VIEWS: Env.schema.boolean(),
